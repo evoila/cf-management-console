2931a6be7e5bc8e21742225e97b95bb599a54a59
Rijn, RA van - SPLXL <Ronald-van.Rijn@klm.com>
2013-09-18 16:00:21 +0200

    Added service instance and plan to application resource

--- grails-app/services/com/github/kratos/service/CloudFoundryService.groovy
+++ grails-app/services/com/github/kratos/service/CloudFoundryService.groovy
@@ -17,7 +17,7 @@ class CloudFoundryService implements InitializingBean {
     }
 
     def getApplication(token, id) {
-        def response = api.get(path: '/v2/apps/'.concat(id), headers: [authorization: token], query: ['inline-relations-depth':'2'])
+        def response = api.get(path: '/v2/apps/'.concat(id), headers: [authorization: token], query: ['inline-relations-depth':'3'])
 
         def buildpack = response.entity.buildpack ? response.entity.buildpack : response.entity.detected.buildpack
 
@@ -28,8 +28,19 @@ class CloudFoundryService implements InitializingBean {
             urls.add(host.concat('.').concat(domain))
         }
 
+        def serviceBindings = []
+        for (binding in response.entity.service_bindings) {
+            def plan = binding.entity.service_instance.entity.service_plan
+            def servicePlan = [id: plan.metadata.guid, name: plan.entity.name, description: plan.entity.description]
+
+            def instance = binding.entity.service_instance
+            def serviceInstance = [id: instance.metadata.guid, name: instance.entity.name, servicePlan: servicePlan]
+
+            serviceBindings.add([id: binding.metadata.guid, serviceInstance: serviceInstance])
+        }
+
         [id: response.metadata.guid, name: response.entity.name, memory: response.entity.memory, diskQuota: response.entity.disk_quota,
-                state: response.entity.state, buildpack: buildpack, urls: urls]
+                state: response.entity.state, buildpack: buildpack, urls: urls, serviceBindings: serviceBindings]
     }
 
 }
