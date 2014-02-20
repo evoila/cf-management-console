074ce0d370cc14d51e3483edcdf2641e528950cd
Johannes Hiemer <jvhiemer@gmail.com>
2014-02-20 13:06:16 +0100

    Marketplace Controller added service plan view.

--- src/main/webapp/WEB-INF/js/controllers/general/marketPlaceController.js
+++ src/main/webapp/WEB-INF/js/controllers/general/marketPlaceController.js
@@ -6,10 +6,15 @@ define(function () {
 	'use strict';	
 	
 	function MarketPlaceController($scope, Restangular) {
+		$scope.service = null;
 
 		Restangular.all('services').getList().then(function (services) {
 			$scope.services = services;				
 		});
+
+		$scope.showDetails = function(service) {
+			$scope.service = service;
+		};
 	}
 
 	MarketPlaceController.$inject = ['$scope', 'Restangular'];
