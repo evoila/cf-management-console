e75320a00448a5960801d9bf13e742cb899ddc96
Johannes Hiemer <jvhiemer@gmail.com>
2014-07-07 08:21:40 +0200

    Small cleanup

--- src/main/webapp/WEB-INF/js/bootstrap.js
+++ src/main/webapp/WEB-INF/js/bootstrap.js
@@ -7,7 +7,12 @@ define(['require', 'angular', 'app'], function(require, angular, app) {
   'use strict';
   
   return require(['domReady!'], function(document) {
-  	console.log('bootstrap.js - called');
-    return angular.bootstrap(document, ['myApp']);
+	console.log('bootstrap.js - called');	
+	try {
+		angular.bootstrap(document, ['myApp']);
+	}
+	catch (e) {
+		console.error(e.stack || e.message || e);
+	}
   });
 });
