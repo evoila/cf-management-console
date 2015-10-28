3e08c5fb8a25db9bada69e134c65178624fff0d6
Christian Müller <chphmu@gmail.com>
2015-10-28 17:48:48 +0100

    Small fixes for broken images

--- src/main/webapp/WEB-INF/app/js/services/general/designService.js
+++ src/main/webapp/WEB-INF/app/js/services/general/designService.js
@@ -25,14 +25,18 @@ angular.module('services')
         return intToRGB(hashCode(name));
       },
       resolveServicePng: function(serviceName) {
-        if (serviceName.indexOf("postgresql") > -1)
-          return "postgresql";
-        if (serviceName.indexOf("mongodb") > -1)
-          return "mongodb";
-        if (serviceName.indexOf("rabbitmq") > -1)
-          return "rabbitmq";
-        if (serviceName.indexOf("redis") > -1)
-          return "redis";
+        if (serviceName != undefined) {
+          if (serviceName.indexOf("postgresql") > -1)
+            return "postgresql";
+          if (serviceName.indexOf("mongodb") > -1)
+            return "mongodb";
+          if (serviceName.indexOf("rabbitmq") > -1)
+            return "rabbitmq";
+          if (serviceName.indexOf("redis") > -1)
+            return "redis";
+        } else {
+          return "notfound";
+        }
       }
     };
     return design;
