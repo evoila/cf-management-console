f57a8172440614e3c7b9a7b4a6a6fa179319f2f2
Johannes Hiemer <jhiemer@evoila.de>
2015-11-23 12:45:00 +0100

    Auto reload of state if the target is same as the current.

--- src/main/webapp/WEB-INF/app/js/services/general/responseService.js
+++ src/main/webapp/WEB-INF/app/js/services/general/responseService.js
@@ -5,8 +5,13 @@ angular.module('services')
   .factory('responseService', function responseService(messageEmitter, $state) {
     var response = {};
     response.success = function(response, message, redirect, params) {
-      if (redirect)
-        $state.go(redirect, params);
+      if (redirect) {
+        if ($state.current && $state.current.name == redirect)
+          $state.go(redirect, params, {reload:true});
+        else
+          $state.go(redirect, params);
+      }
+
       messageEmitter.message(message);
     },
     response.error = function(response, message) {
